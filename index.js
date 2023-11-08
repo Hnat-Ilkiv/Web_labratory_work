5925e746e890a28214b547950a9773303290fe8f
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-11-08 11:56:15 +0200

    Fixes for minor editing and searching and sorting errors

--- index.js
+++ index.js
@@ -22,6 +22,22 @@ async function displayItem() {
                           "";
 }
 
+function show_items(items) {
+    itemPlace.innerHTML = (items.length  > 0) ?
+                          items.map(item =>
+                              `
+                              <li>
+                              <div class="preview_video"></div>
+                              <h3>${item.title}</h3>
+                              <p>Space: ${item.spaseMb} MB </p>
+                              <p>Time: ${~~(item.timeSecond / 60)} m ${item.timeSecond % 60} s</p>
+                              <button onclick="editeItems('${item.id}')">Edite Video</button>
+                              <button onclick="deleteItem('${item.id}')">Delete Video</button>
+                              </li>
+                              `).join("") :
+                          "";
+}
+
 function getItemList() {
     return fetch('http://localhost:3000/item')
         .then(response => response.json())
@@ -100,6 +116,8 @@ function saveItems(item_id) {
             .then(data => {
                 console.log(data);
                 // оновлення локальних даних або виклик інших функцій
+                document.getElementById('edite_modal').style.display = 'none';
+                displayItem();
             })
             .catch(error => console.error('Error:', error));
     }
@@ -123,92 +141,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
     displayItem();
 });
 
-// function createItems() {
-//     const name = document.getElementById("video_name_create_input").value;
-//     if(items_list.some(item => item.title == name)){
-//         alert("An element with that name already exists.")
-//     }
-//     else if (name == "") {
-//         alert("Name is missing.")
-//     }
-//     else {
-//         const size = document.getElementById("used_spase_mb_create_input").value;
-//         const time = document.getElementById("video_time_s_create_input").value;
-//         const new_item = {
-//             title: name,
-//             used_spase_mb: size,
-//             video_time_s: time
-//         };
+
 //
-//         items_list.push(new_item);
+function searchItems() {
+    console.log("search items")
+    const inputContainer = document.getElementById("inputSearchId").value.toLowerCase();
+
+      show_items(itemList.filter((item) => {
+        return item.title.toLowerCase().includes(inputContainer);
+      }));
+}
 //
-//         document.getElementById('create_modal').style.display='none';
-//         show_items(items_list);
-//     }
-// }
-//
-// function editeItems(item_name) {
-//     let element = items_list.find(item => item.title == item_name);
-//
-//     saveButton.innerHTML = `<button class="manager_button" onclick="saveItems('${element.title}')">Save</button>`
-//
-//     document.getElementById('edite_modal').style.display='block';
-//     document.getElementById('video_name_edite_input').value = element.title;
-//     document.getElementById('used_spase_mb_edite_input').value = element.used_spase_mb;
-//     document.getElementById('video_time_s_edite_input').value = element.video_time_s;
-// }
-//
-// function saveItems(item_name){
-//     const name = document.getElementById("video_name_edite_input").value;
-//     const size = document.getElementById("used_spase_mb_edite_input").value;
-//     const time = document.getElementById("video_time_s_edite_input").value;
-//
-//     let element = items_list.find(item => item.title == item_name);
-//
-//     element.title = null;
-//     if(items_list.some(item => item.title == name)){
-//         element.title = item_name;
-//         alert("An element with that name already exists.")
-//     }
-//     else if (name == "") {
-//         alert("Name is missing.")
-//     }
-//     else {
-//         element.used_spase_mb = size;
-//         element.video_time_s = time;
-//
-//         document.getElementById('edite_modal').style.display='none';
-//         saveButton.innerHTML = ``;
-//         show_items(items_list);
-//     }
-// }
-//
-// function searchItems() {
-//     console.log("search items")
-//     const inputContainer = document.getElementById("inputSearchId").value.toLowerCase();
-//
-//       show_items(items_list.filter((item) => {
-//         return item.title.toLowerCase().includes(inputContainer);
-//       }));
-// }
-//
-// function sortItems() {
-//     console.log("sort items");
-//         show_items(items_list.slice().sort((a, b) => {
-//             const titleA = a.title.toUpperCase();
-//             const titleB = b.title.toUpperCase();
-//
-//             if (titleA < titleB) {
-//                 return -1;
-//             }
-//             if (titleA > titleB) {
-//                 return 1;
-//             }
-//
-//             return 0;
-//         }));
-//     }
-// }
+function sortItems() {
+    console.log("sort items");
+        show_items(itemList.slice().sort((a, b) => {
+            const titleA = a.title.toUpperCase();
+            const titleB = b.title.toUpperCase();
+
+            if (titleA < titleB) {
+                return -1;
+            }
+            if (titleA > titleB) {
+                return 1;
+            }
+
+            return 0;
+        }));
+    }
 //
 function countItemsSpase() {
     console.log("count items spase");
